ad285049e1382cb6b339a37fc5f7d0ed5c56a804
Sofia Bargues <barguessofia@gmail.com>
2023-12-03 09:07:38 +0000

    day 2

--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,34 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.ts';
+import { parseLines } from './parse.ts';
 
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const games = parseLines(data);
+  const largests = games.map((game) =>
+    game.reduce(
+      (maxHand, hand) => {
+        if (maxHand.blue < hand.blue) {
+          maxHand.blue = hand.blue;
+        }
+        if (maxHand.green < hand.green) {
+          maxHand.green = hand.green;
+        }
+        if (maxHand.red < hand.red) {
+          maxHand.red = hand.red;
+        }
+        return maxHand;
+      },
+      { blue: 0, green: 0, red: 0 }
+    )
+  );
+  let result = largests.map(
+    (largest) => largest.blue * largest.green * largest.red
+  );
+
+  return result.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
 }
 
 const answer = await day2b();
--- puzzles/day-2/parse.ts
+++ puzzles/day-2/parse.ts
@@ -1,11 +1,28 @@
-export const parseLines = (input: string): number[][] => {
-	return input
-		.split(/\r?\n/)
-		.filter(line => !!line)
-		.map(line =>
-			line
-				.split(',')
-				.filter(c => /^(\+|-)?[0-9]+$/.test(c))
-				.map(a => parseInt(a, 10))
-		);
+export const parseLines = (
+  input: string[]
+): { blue: number; green: number; red: number }[][] => {
+  return input
+    .filter((line) => !!line)
+    .map(
+      (game) =>
+        game
+          .split(':')[1]
+          .trim()
+          .split(';')
+          .map((handfull) =>
+            handfull
+              .split(',')
+              .map((cube) => cube.trim().split(' '))
+              .reduce(
+                (hand, numColor) => {
+                  hand[numColor[1]] = Number(numColor[0]);
+                  return hand;
+                },
+                { blue: 0, green: 0, red: 0 }
+              )
+          )
+      // .split(',')
+      // .filter((c) => /^(\+|-)?[0-9]+$/.test(c))
+      // .map((a) => parseInt(a, 10))
+    );
 };
