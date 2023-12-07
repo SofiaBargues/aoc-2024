8b86c342a595b4b6b70f409354102a50ab65886c
Francisco Moretti <franciscoemoretti@gmail.com>
2023-12-07 16:29:03 +0000

    day 6

--- puzzles/day-6/day-6-a.ts
+++ puzzles/day-6/day-6-a.ts
@@ -1,9 +1,25 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.ts';
+import { parseLines } from './parse.ts';
 
 export async function day6a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const races = parseLines(data);
+  let totalWinWays = 1;
+  for (const race of races) {
+    const { time: totalTime, distance: recordDistance } = race;
+    let winWays = 0;
+    for (let accTime = 0; accTime <= totalTime; accTime++) {
+      const speed = accTime;
+      const travelTime = totalTime - accTime;
+      const distance = speed * travelTime;
+      if (distance > recordDistance) {
+        winWays++;
+      }
+    }
+    totalWinWays *= winWays;
+  }
+  return totalWinWays;
 }
 
 const answer = await day6a();
--- puzzles/day-6/day-6-b.ts
+++ puzzles/day-6/day-6-b.ts
@@ -1,10 +1,36 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.ts';
+import { Race, parseLines } from './parse.ts';
 
 export async function day6b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const races = parseLines(data);
+  let totalWinWays = 1;
+
+  for (const race of races) {
+    totalWinWays = getWinningWays(race, totalWinWays);
+  }
+  return totalWinWays;
 }
 
 const answer = await day6b();
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+
+function getWinningWays(race: Race, totalWinWays: number) {
+  const { time: totalTime, distance: recordDistance } = race;
+  let winWays = 0;
+  const calculationPercent = totalTime / 100;
+  for (let accTime = 0; accTime <= totalTime; accTime++) {
+    if (accTime % calculationPercent == 0) {
+      console.log(accTime / calculationPercent + ' percent');
+    }
+    const speed = accTime;
+    const travelTime = totalTime - accTime;
+    const distance = speed * travelTime;
+    if (distance > recordDistance) {
+      winWays++;
+    }
+  }
+  totalWinWays *= winWays;
+  return totalWinWays;
+}
