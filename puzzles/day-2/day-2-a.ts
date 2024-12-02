4baa2d5e2400c9d36baf07e483616bb78fb5731f
Sofia Bargues <barguessofia@gmail.com>
2024-12-02 22:23:18 +0000

    day2.a

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -4,7 +4,39 @@ import { parseLines } from './parse.ts';
 
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  parseLines(data);
+  const reports = parseLines(data);
+
+  // -1. let acc
+  let acc = 0;
+  // 0.itero a travez de los reportes
+  for (const report of reports) {
+    // 1.chequeo el orden => asc
+    let asc = report[0] < report[1];
+    //     1.a let isSafe=true
+    let isSafe = true;
+    //     2.itero tomando 2 val consecutivos
+    for (let i = 0; i < report.length - 1; i++) {
+      // 1.a calcular la differencia:
+      //              if ( asc )not la diferencia del segundo menos
+      //             else not la diferencia del primerp menos
+      let diff = asc ? report[i + 1] - report[i] : report[i] - report[i + 1];
+      //          1.b checkear si no es valida
+      //                 diff >=1 y  diff  <=3
+      if (!(diff >= 1 && diff <= 3)) {
+        //             2.a isSafe=false
+        //             2.breack
+        isSafe = false;
+        break;
+      }
+    }
+    //
+    //      3. si isSafe acc++
+    if(isSafe){
+      
+      acc++
+    }
+  }
+  return acc;
 }
 
 const answer = await day2a();
--- puzzles/day-2/parse.ts
+++ puzzles/day-2/parse.ts
@@ -1,28 +1,3 @@
-export const parseLines = (
-  input: string[]
-): { blue: number; green: number; red: number }[][] => {
-  return input
-    .filter((line) => !!line)
-    .map(
-      (game) =>
-        game
-          .split(':')[1]
-          .trim()
-          .split(';')
-          .map((handfull) =>
-            handfull
-              .split(',')
-              .map((cube) => cube.trim().split(' '))
-              .reduce(
-                (hand, numColor) => {
-                  hand[numColor[1]] = Number(numColor[0]);
-                  return hand;
-                },
-                { blue: 0, green: 0, red: 0 }
-              )
-          )
-      // .split(',')
-      // .filter((c) => /^(\+|-)?[0-9]+$/.test(c))
-      // .map((a) => parseInt(a, 10))
-    );
+export const parseLines = (input: string[]) => {
+  return input.map((line) => line.split(' ').map(Number));
 };
