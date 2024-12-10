4e8b500bb0e04047b648d286e6eda345dab602f8
Sofia Bargues <barguessofia@gmail.com>
2024-12-10 13:33:33 +0000

    das-10

--- puzzles/day-10/day-10-b.ts
+++ puzzles/day-10/day-10-b.ts
@@ -1,184 +1,77 @@
 import chalk from 'chalk';
 import { readLines } from '../../shared.ts';
-import {
-  Grid,
-  PipeType,
-  Position,
-  findStartPosition,
-  parseLines,
-} from './parse.ts';
+import { parseLines } from './parse.ts';
 
-function getNeighbours(grid: Grid, pos: Position): Position[] {
-  const { x, y } = pos;
-  const pipe = getCellPipe(grid, pos);
-
-  if (pipe == PipeType.Vertical) {
-    return [
-      { x, y: y + 1 },
-      { x, y: y - 1 },
-    ];
-  } else if (pipe == PipeType.Horizontal) {
-    return [
-      { y, x: x + 1 },
-      { y, x: x - 1 },
-    ];
-  } else if (pipe == PipeType.F) {
-    return [
-      { y: y + 1, x: x },
-      { y, x: x + 1 },
-    ];
-  } else if (pipe == PipeType._7) {
-    return [
-      { x, y: y + 1 },
-      { y, x: x - 1 },
-    ];
-  } else if (pipe == PipeType.L) {
-    return [
-      { y: y - 1, x },
-      { y, x: x + 1 },
-    ];
-  } else if (pipe == PipeType.J) {
-    return [
-      { y, x: x - 1 },
-      { y: y - 1, x },
-    ];
-  } else if (pipe == PipeType.Start) {
-    return getStartNeighbours(grid, pos);
+function findTrailHeads(grid: number[][]): number[][] {
+  let heads = [];
+  let n = grid.length;
+  let m = grid[0].length;
+  //   for i 0...n-1
+  for (let i = 0; i <= n - 1; i++) {
+    for (let j = 0; j <= m - 1; j++) {
+      if (grid[i][j] === 0) heads.push([i, j]);
+    }
   }
-
-  return [];
+  return heads;
 }
 
-function getNextNeighbour(
-  grid: Grid,
-  pos: Position,
-  previousPos: Position
-): Position {
-  const neighbours = getNeighbours(grid, pos);
-  return arePosEqual(neighbours[0], previousPos)
-    ? neighbours[1]
-    : neighbours[0];
+function isInGrid(pos: number[], grid: number[][]) {
+  let n = grid.length;
+  let m = grid[0].length;
+
+  let [x, y] = pos;
+  return x >= 0 && x < n && y >= 0 && y < m;
 }
 
-function arePosEqual(pos1: Position, pos2: Position) {
-  return pos1.x == pos2.x && pos1.y == pos2.y;
-}
-
-export function getStartNeighbours(grid: Grid, startPos: Position): Position[] {
-  const { x, y } = startPos;
-  const allNeighbours = [
-    { x, y: y - 1 },
-    { x, y: y + 1 },
-    { x: x + 1, y },
-    { x: x - 1, y },
+function getGoodTrailCount(grid, initPos): number {
+  let dirs = [
+    [0, +1],
+    [+1, 0],
+    [-1, 0],
+    [0, -1],
   ];
+  let founds = 0;
+  function walkTrails(pos, prev: number) {
+    //     // base condition
+    if (!isInGrid(pos, grid)) {
+      return;
+    }
 
-  return allNeighbours.filter((neighbourPos) =>
-    getNeighbours(grid, neighbourPos)
-      .map((pos) => getCellPipe(grid, pos))
-      .includes(PipeType.Start)
-  );
-}
+    let val = grid[pos[0]][pos[1]];
+    if (val != prev + 1) {
+      return;
+    }
 
-export function getCellPipe(grid: Grid, pos: Position): PipeType | undefined {
-  if (grid[pos.y]) {
-    return grid[pos.y][pos.x];
+    if (val === 9) {
+      founds++;
+      return;
+    }
+    //     // recurse
+    for (const dir of dirs) {
+      const newPos = [pos[0] + dir[0], pos[1] + dir[1]];
+      walkTrails(newPos, val);
+    }
   }
-}
-
-function getStartPipeType(grid: Grid, startPos: Position): PipeType {
-  const neighbours = getStartNeighbours(grid, startPos);
-  const up = neighbours.filter((pos) => pos.y == startPos.y - 1).length == 1;
-  const down = neighbours.filter((pos) => pos.y == startPos.y + 1).length == 1;
-  const left = neighbours.filter((pos) => pos.x == startPos.x - 1).length == 1;
-  const right = neighbours.filter((pos) => pos.x == startPos.x + 1).length == 1;
-  if (up && down) {
-    return PipeType.Vertical;
-  } else if (left && right) {
-    return PipeType.Horizontal;
-  } else if (left && up) {
-    return PipeType.J;
-  } else if (left && down) {
-    return PipeType._7;
-  } else if (right && up) {
-    return PipeType.L;
-  } else if (right && down) {
-    return PipeType.F;
-  } else {
-    throw Error('Cant determine start value');
-  }
+  walkTrails(initPos, -1);
+  return founds;
 }
 
 export async function day10b(dataPath?: string) {
   const data = await readLines(dataPath);
   const grid = parseLines(data);
-  const startPos = findStartPosition(grid);
 
-  let prevPos = { ...startPos };
-  let pos = getStartNeighbours(grid, startPos)[0];
-  const loop = [pos];
-  while (!arePosEqual(pos, startPos)) {
-    const next = getNextNeighbour(grid, pos, prevPos);
-    loop.push(next);
-    prevPos = pos;
-    pos = next;
+  // encuentro los trailhead
+  let headLocations = findTrailHeads(grid);
+  // para cada trailHead
+  let acc = 0;
+  for (const headLocation of headLocations) {
+    //     cuantos 9 distintos alcanza
+    //     acc los 9 alcanzados
+    acc += getGoodTrailCount(grid, headLocation);
   }
+  // retorno el total
 
-  // Replace Start pipe type
-  grid[startPos.y][startPos.x] = getStartPipeType(grid, startPos);
-  const rowInnerCellCounts = grid.map((row, y) => {
-    return calculateRowInnerCells(row, loop, y);
-  });
-  console.log(rowInnerCellCounts);
-  return rowInnerCellCounts.reduce(
-    (acc, val) => acc + val.reduce((accRow, valRow) => accRow + valRow, 0),
-    0
-  );
-}
-
-function calculateRowInnerCells(
-  row: PipeType[],
-  loop: Position[],
-  y: number
-): number[] {
-  let nestCellsCount = 0;
-  let nestRow = [];
-  let previousBend = null;
-  let isInsideLoop = false;
-  row.forEach((cell, x) => {
-    const isLoopCell = isPosPartOfLoop(loop, { x, y });
-    if (isLoopCell) {
-      if (cell == PipeType.Vertical) {
-        isInsideLoop = !isInsideLoop;
-      }
-      if ([PipeType.F, PipeType.L].includes(cell)) {
-        previousBend = cell;
-      }
-      if (cell == PipeType._7) {
-        if (previousBend == PipeType.L) {
-          isInsideLoop = !isInsideLoop;
-        }
-        previousBend = null;
-      }
-      if (cell == PipeType.J) {
-        if (previousBend == PipeType.F) {
-          isInsideLoop = !isInsideLoop;
-        }
-        previousBend = null;
-      }
-      nestRow.push(0);
-    } else if (isInsideLoop) {
-      nestCellsCount++;
-      nestRow.push(1);
-    } else {
-      nestRow.push(0);
-    }
-  });
-  return nestRow;
-}
-
-function isPosPartOfLoop(loop: Position[], pos: Position) {
-  return loop.filter((loopPos) => arePosEqual(loopPos, pos)).length == 1;
+  return acc;
 }
 
 const answer = await day10b();
--- puzzles/day-10/parse.ts
+++ puzzles/day-10/parse.ts
@@ -1,44 +1,5 @@
-export enum PipeType {
-  Vertical = '|',
-  Horizontal = '-',
-  F = 'F',
-  _7 = '7',
-  L = 'L',
-  J = 'J',
-  Ground = '.',
-  Start = 'S',
-}
-
-export type Position = {
-  x: number;
-  y: number;
-};
-
-export type Grid = PipeType[][];
-
-export const parseLines = (lines: string[]): Grid => {
+export const parseLines = (lines: string[]) => {
   lines = lines.filter((line) => !!line);
 
-  return lines.map((line) =>
-    line.split('').map((pipeLetter) => {
-      const pipeTypeValue = Object.values(PipeType).find(
-        (value) => value === pipeLetter
-      ) as PipeType | undefined;
-
-      if (pipeTypeValue) {
-        return pipeTypeValue;
-      } else {
-        throw Error('Wrong input letter');
-      }
-    })
-  );
+  return lines.map((line) => line.split('').map(Number));
 };
-
-export function findStartPosition(grid: Grid): Position {
-  const y = grid.findIndex((row) => row.includes(PipeType.Start));
-  const x = grid[y].findIndex((cell) => cell == PipeType.Start);
-  return {
-    x,
-    y,
-  };
-}
