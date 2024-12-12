c69a288970a866d19bd8dc2db24cf325958b49f7
Sofia Bargues <barguessofia@gmail.com>
2024-12-12 22:22:59 +0000

    day 12 part a 1/2

--- puzzles/day-12/day-12-a.ts
+++ puzzles/day-12/day-12-a.ts
@@ -1,80 +1,83 @@
 import chalk from 'chalk';
 import { readLines } from '../../shared.ts';
-import { Conditions, Groups, parseLines } from './parse.ts';
-
-function getAllPlacements(conditions: Conditions, groupLength: number) {
-  const placements: number[] = [];
-  for (let i = 0; i <= conditions.length - groupLength; i++) {
-    const isLeftValid = i - 1 < 0 || conditions[i - 1] != '#';
-    const isRightValid =
-      i + groupLength > conditions.length - 1 ||
-      conditions[i + groupLength] != '#';
-    const areGroupSpringsValid = conditions
-      .slice(i, i + groupLength)
-      .every((condition) => condition == '#' || condition == '?');
-
-    const noBrokenBeforeIt = conditions
-      .slice(0, i)
-      .every((condition) => condition != '#');
-
-    const isValid =
-      isLeftValid && isRightValid && areGroupSpringsValid && noBrokenBeforeIt;
-    if (isValid) {
-      placements.push(i);
-    }
-  }
-  return placements;
+import { parseLines } from './parse.ts';
+let dirs = [
+  [-1, 0],
+  [1, 0],
+  [0, 1],
+  [0, -1],
+];
+function isInGrid(grid: string[][], pos: number[]) {
+  let n = grid.length;
+  let m = grid[0].length;
+  let [x, y] = pos;
+  return x >= 0 && x <= n - 1 && y >= 0 && y <= m - 1;
 }
 
-function getArrangementCounts(conditions: Conditions, groups: Groups): number {
-  const firstBrokenGroupLength = groups[0];
+function walk(
+  region: Set<string>,
+  initVal: string,
+  grid: string[][],
+  pos: number[]
+) {
+  // Base condition
+  if (region.has(JSON.stringify(pos))) {
+    return;
+  }
+  if (!isInGrid(grid, pos)) {
+    return;
+  }
+  let val = grid[pos[0]][pos[1]];
+  if (val !== initVal) {
+    return;
+  }
+  region.add(JSON.stringify(pos));
+  // Recursion
+  for (const [dx, dy] of dirs) {
+    let [x, y] = pos;
+    let newPos = [x + dx, y + dy];
+    walk(region, initVal, grid, newPos);
+  }
+}
 
-  const firstGroupPlacements = getAllPlacements(
-    conditions,
-    firstBrokenGroupLength
-  );
-  // console.log({ conditions, firstBrokenGroupLength, firstGroupPlacements });
+function findRegion(initPos: number[], grid: string[][]): number[][] {
+  let region: Set<string> = new Set();
+  let initVal = grid[initPos[0]][initPos[1]];
+  walk(region, initVal, grid, initPos);
+  return [...region].map((x) => JSON.parse(x));
+}
 
-  const nextGroupCount = firstGroupPlacements
-    .map((groupStart) => {
-      const firstGroupEnd = groupStart + firstBrokenGroupLength;
-      const nextGroups = groups.slice(1);
-      const conditionsSubarray = conditions.slice(firstGroupEnd + 1);
-
-      if (nextGroups.length == 0) {
-        const noBrokenAfterIt =
-          !conditionsSubarray ||
-          conditions
-            .slice(firstGroupEnd)
-            .every((condition) => condition != '#');
-        if (noBrokenAfterIt) {
-          return 1;
-        } else {
-          return 0;
-        }
+function findRegions(grid: string[][]): number[][][] {
+  let regions: number[][][] = [];
+  let seen = new Set();
+  let n = grid.length;
+  let m = grid[0].length;
+  for (let x = 0; x <= n - 1; x++) {
+    for (let y = 0; y <= m - 1; y++) {
+      if (seen.has(JSON.stringify([x, y]))) {
+        continue;
       }
-
-      const nextGroupsCount = getArrangementCounts(
-        conditionsSubarray,
-        nextGroups
-      );
-
-      return nextGroupsCount;
-    })
-    .reduce((acc, curr) => acc + curr, 0);
-  return nextGroupCount;
+      let region = findRegion([x, y], grid);
+      region.forEach((x: number[]) => seen.add(JSON.stringify(x)));
+      regions.push(region);
+      // console.log(grid[x][y]);
+    }
+  }
+  return regions;
 }
 
 export async function day12a(dataPath?: string) {
   const data = await readLines(dataPath);
-  let springRecords = parseLines(data);
-  // console.log(orderData);
-  const springRecordsArrangements = springRecords
-    .map((record) =>
-      getArrangementCounts(record.conditions, record.brokenGroups)
-    )
-    .reduce((acc, curr) => acc + curr, 0);
-  return springRecordsArrangements;
+  let garden = parseLines(data);
+  // 1. encontrar las regiones
+  let regions = findRegions(garden);
+  // 2. area y perimetro de cada region
+  //     area= cantida de pots
+  let area = regions.map((r)=>r.length);
+  //     perimetro= vecesque encuentrto un vecino chequeado desde cada pot
+  // 3. para cada region total += area * perimetro
+  console.log(area);
+  return 0;
 }
 
 const answer = await day12a();
--- puzzles/day-12/parse.ts
+++ puzzles/day-12/parse.ts
@@ -1,22 +1,7 @@
-export type Conditions = string[];
 
-export type Groups = number[];
 
-export type SpringRecord = {
-  conditions: Conditions;
-  brokenGroups: Groups;
-};
-
-export const parseLines = (lines: string[]): SpringRecord[] => {
+export const parseLines = (lines: string[]): string[][] => {
   lines = lines.filter((line) => !!line);
 
-  return lines.map((line) => {
-    const [firstPart, secondPart] = line.split(' ');
-    return {
-      conditions: firstPart.split(''),
-      brokenGroups: secondPart
-        .split(',')
-        .map((element) => parseInt(element, 10)),
-    };
-  });
-};
+return lines.map((line)=>line.split("")) 
+}
